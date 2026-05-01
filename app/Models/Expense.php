bf743bbc564b16e8aef8ed95c835d5eeb9b42c63
Ramson Lonayo <ramsonlonayo@gmail.com>
2026-05-01 18:36:15 +0300

    Configure expense model fillable fields and enable expense insertion

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Override;
 
 class Expense extends Model
 {
-    //
+  protected $fillable = [
+    'title',
+    'amount',
+    'category',
+    'expense_date',
+    'description',
+  ];
 }
