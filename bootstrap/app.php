069e6c3b3d91408da36722058131772261a4a11f
Ramson Lonayo <ramsonlonayo@gmail.com>
2026-05-04 16:49:03 +0300

    Protect API routes using Sanctum authentication middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        $middleware->redirectGuestsTo(function () {
+        return null;
+        });
+
     })
+        //
+    
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ExpenseController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('expenses', ExpenseController::class);
+
+});
