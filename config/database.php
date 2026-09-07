73c8f8097d4828bbbc16f1458e28f95d4e3107c9
Ramson Lonayo <ramsonlonayo@gmail.com>
2026-09-07 16:45:42 +0300

    security: verify Aiven MySQL server certificate

--- config/database.php
+++ config/database.php
@@ -59,10 +59,14 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            
             'options' => extension_loaded('pdo_mysql') ? [
-    PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-] : [],
+              PDO::MYSQL_ATTR_SSL_CA => env(
+                'MYSQL_ATTR_SSL_CA',
+                '/etc/secrets/aiven-ca.pem'
+            ),
+           PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+        ] : [],
         ],
 
         'mariadb' => [
